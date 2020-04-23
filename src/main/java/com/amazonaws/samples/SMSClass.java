ebf62f1d003b556d678722947b22ab20fbcd4b3b
anhad123 <anhad29saran1995@gmail.com>
2020-04-24 00:11:39 +0530

    try catch

--- src/main/java/com/amazonaws/samples/SMSClass.java
+++ src/main/java/com/amazonaws/samples/SMSClass.java
@@ -30,80 +30,81 @@ public class SMSClass {
 
 	private synchronized void waitMethod() throws InterruptedException, IOException {
 		while(true) {
-			System.out.println("always running program ==> " + Calendar.getInstance().getTime());
-			String finalUrl="https://esatsang.itas.in/auth/login?next=%2F";	
-			URL obj = new URL(finalUrl);
-			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-			con.setRequestMethod("GET");
-			int responseCode = con.getResponseCode();
-			System.out.println("GET Response Code :: " + responseCode);
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			String response1=response.toString();
-			System.out.println(response1);
-			if(!response1.contains("Login is only allowed after the transmission starts."))
-			{
-				String finalUrl2="https://esatsang.itas.in/9nUCp0IbDlFoaudioout/live.m3u8";	
-				URL obj2 = new URL(finalUrl2);
-				HttpsURLConnection con2 = (HttpsURLConnection) obj2.openConnection();
-				//con.setRequestMethod("GET");
-				int responseCode2 = con2.getResponseCode();
-				System.out.println("inner Response Code "+responseCode2);
-				if(responseCode2!=404) {
-					BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJJ2YKDFFGSX6CJOQ", "Y+pOQVIsH82R9GBKr4SpTmt7+U1VUFRJ9DZtE1dr");
-					String SMSMessage = "Radhasoami, Login Enabled For E-Satsang Radhasoami";
-					String[] mobileList = {
-							"+919087772871","+919452290492","+918318160991","+919176745704","+919962388429","+919087772871"};
-					for(String mobile:mobileList) {
-						AmazonSNS snsClient = AmazonSNSClient
-								.builder()
-								.withRegion(Regions.AP_SOUTH_1)
-								.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-								.build();							
-						Map<String, MessageAttributeValue> smsAttributes = 
-								new HashMap<String, MessageAttributeValue>();
-						smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
-								.withStringValue("Transactional") //Sets the type to promotional.
-								.withDataType("String"));
-						//smsAttributes.put("AWS.SNS.SMS.SenderID",new MessageAttributeValue().withStringValue("RS123").withDataType("String"));
-						PublishResult result =snsClient.publish(new PublishRequest().withMessage(SMSMessage).withPhoneNumber(mobile).withMessageAttributes(smsAttributes));
-						System.out.println(result);
-					}
+			try {
+				System.out.println("always running program ==> " + Calendar.getInstance().getTime());
+				String finalUrl="https://esatsang.itas.in/auth/login?next=%2F";	
+				URL obj = new URL(finalUrl);
+				HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+				con.setRequestMethod("GET");
+				int responseCode = con.getResponseCode();
+				System.out.println("GET Response Code :: " + responseCode);
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+				String response1=response.toString();
+				System.out.println(response1);
+				if(!response1.contains("Login is only allowed after the transmission starts."))
+				{
+					String finalUrl2="https://esatsang.itas.in/9nUCp0IbDlFoaudioout/live.m3u8";	
+					URL obj2 = new URL(finalUrl2);
+					HttpsURLConnection con2 = (HttpsURLConnection) obj2.openConnection();
+					//con.setRequestMethod("GET");
+					int responseCode2 = con2.getResponseCode();
+					System.out.println("inner Response Code "+responseCode2);
+					if(responseCode2!=404) {
+						BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJJ2YKDFFGSX6CJOQ", "Y+pOQVIsH82R9GBKr4SpTmt7+U1VUFRJ9DZtE1dr");
+						String SMSMessage = "Radhasoami, Login Enabled For E-Satsang Radhasoami";
+						String[] mobileList = {
+								"+919087772871","+919452290492","+918318160991","+919176745704","+919962388429","+919087772871"};
+						for(String mobile:mobileList) {
+							AmazonSNS snsClient = AmazonSNSClient
+									.builder()
+									.withRegion(Regions.AP_SOUTH_1)
+									.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+									.build();							
+							Map<String, MessageAttributeValue> smsAttributes = 
+									new HashMap<String, MessageAttributeValue>();
+							smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
+									.withStringValue("Transactional") //Sets the type to promotional.
+									.withDataType("String"));
+							//smsAttributes.put("AWS.SNS.SMS.SenderID",new MessageAttributeValue().withStringValue("RS123").withDataType("String"));
+							PublishResult result =snsClient.publish(new PublishRequest().withMessage(SMSMessage).withPhoneNumber(mobile).withMessageAttributes(smsAttributes));
+							System.out.println(result);
+						}
 
 
-					String[] mobileList2= {"+919841586929","+13108966180","+919566145196","+919314436433","+919087772871"};
-					for(String mobile:mobileList2)
-					{
-						AmazonSNS snsClient = AmazonSNSClient
-								.builder()
-								.withRegion(Regions.US_WEST_2)
-								.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-								.build();							
-						Map<String, MessageAttributeValue> smsAttributes = 
-								new HashMap<String, MessageAttributeValue>();
-						smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
-								.withStringValue("Transactional") //Sets the type to promotional.
-								.withDataType("String"));
-						//smsAttributes.put("AWS.SNS.SMS.SenderID",new MessageAttributeValue().withStringValue("RS123").withDataType("String"));
-						PublishResult result =snsClient.publish(new PublishRequest().withMessage(SMSMessage).withPhoneNumber(mobile).withMessageAttributes(smsAttributes));
-						System.out.println(result);
-						//this.wait(10000);
-					}
-					/*Runtime runtime = Runtime.getRuntime();
+						String[] mobileList2= {"+919841586929","+13108966180","+919566145196","+919314436433","+919087772871"};
+						for(String mobile:mobileList2)
+						{
+							AmazonSNS snsClient = AmazonSNSClient
+									.builder()
+									.withRegion(Regions.US_WEST_2)
+									.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+									.build();							
+							Map<String, MessageAttributeValue> smsAttributes = 
+									new HashMap<String, MessageAttributeValue>();
+							smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
+									.withStringValue("Transactional") //Sets the type to promotional.
+									.withDataType("String"));
+							//smsAttributes.put("AWS.SNS.SMS.SenderID",new MessageAttributeValue().withStringValue("RS123").withDataType("String"));
+							PublishResult result =snsClient.publish(new PublishRequest().withMessage(SMSMessage).withPhoneNumber(mobile).withMessageAttributes(smsAttributes));
+							System.out.println(result);
+							//this.wait(10000);
+						}
+						/*Runtime runtime = Runtime.getRuntime();
 					BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 					Process proc = runtime.exec("shutdown -s -t "+(Long.parseLong("120")));
 					System.exit(0);*/
-					this.wait(7200000);
+						this.wait(7200000);
+					}
+					else {
+						System.out.println("inner not");
+					}
 				}
-				else {
-					System.out.println("inner not");
-				}
-			}
 				else
 				{
 					System.out.println("outer not");
@@ -114,5 +115,12 @@ public class SMSClass {
 					e.printStackTrace();
 				}
 			}
+
+			catch (Exception e)
+			{
+				System.out.println(e.getLocalizedMessage());
+				this.wait(10000);
+			}
 		}
 	}
+}
